e5eb62bf666f2b16c05dca1a8407e8fdbb40993b
Ryan Gard <ryan.m.gard@gmail.com>
2019-04-15 23:30:16 -0500

    initial commit

---
+++ build/precache-manifest.ef6ad9c5b23b87ff588eefda72477a63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "28b436ecfa58e3d9b006",
+    "url": "/static/js/main.652d73cb.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "28b436ecfa58e3d9b006",
+    "url": "/static/css/main.f71b982e.chunk.css"
+  },
+  {
+    "revision": "25cfaeaafc6c4270c536a64e9a2cbdb1",
+    "url": "/index.html"
+  }
+];
